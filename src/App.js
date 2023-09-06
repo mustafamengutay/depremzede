e9fcebc1b317c789234d8f89574953f1b99d1fde
Mustafa <mustafamengutayy@gmail.com>
2023-09-06 11:19:45 +0300

    feat: create home route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div className="App">
+    <div className="container">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
