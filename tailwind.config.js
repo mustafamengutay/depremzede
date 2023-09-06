bb03b6b11446cd03ab49ec8fde381cf6d0bcb6a1
Mustafa <mustafamengutayy@gmail.com>
2023-09-06 10:45:53 +0300

    chore: add common styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      xl: '1160px',
+    },
+    container: {
+      center: true,
+    },
+    fontFamily: {
+      'sans': ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        white: '#F8F9FA',
+        black: '#141414',
+      },
+      fontSize: {
+        base: '0.875rem',
+      },
+      letterSpacing: {
+        'minus-4': '-0.25rem',
+      }
+    },
   },
   plugins: [],
 }
