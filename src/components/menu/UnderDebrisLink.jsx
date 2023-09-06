041ce0a0c4f86cf4b098e2c6faca33c9c96ef69d
Mustafa <mustafamengutayy@gmail.com>
2023-09-06 14:29:19 +0300

    feat: create UnderDebrisLink menu link

---
+++ src/components/menu/UnderDebrisLink.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import megaphone from '../../assets/megaphone.svg';
+
+const UnderDebrisLink = () => {
+  return (
+    <Link className="btn-big">
+      <div className="relative h-full flex items-start justify-between">
+        <div className="w-full flex justify-between items-center">
+          <img src={megaphone} alt="" />
+          <div className="flex flex-col items-center justify-between h-full">
+            <p className="font-bold text-2xl w-[174px] h-[54px] mr-2">
+              Ben/Tanıdığım Enkaz Altında
+            </p>
+            <div className="bg-black whitespace-nowrap text-white px-8 py-2 rounded-full absolute bottom-0">
+              Yardım Formu
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+export default UnderDebrisLink;
