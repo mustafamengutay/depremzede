f164885aaeabf96fbf3b62076af16a12a0543be2
Mustafa <mustafamengutayy@gmail.com>
2023-09-09 16:05:43 +0300

    feat: create exit button

---
+++ src/components/layouts/CloseButton.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import cross from '../../assets/cross.svg';
+
+const CloseButton = () => {
+  return (
+    <Link
+      className="h-8 w-8 flex items-center justify-center border border-grey-2 rounded-lg"
+      to="/"
+    >
+      <img src={cross} alt="A cross" />
+    </Link>
+  );
+};
+
+export default CloseButton;
