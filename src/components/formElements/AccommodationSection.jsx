1cd142f7e73173970f12fd682072da8f3b553eb1
Mustafa <mustafamengutayy@gmail.com>
2023-09-09 20:52:59 +0300

    feat: create accommodation section

---
+++ src/components/formElements/AccommodationSection.jsx
@@ -0,0 +1,25 @@
+import Input from './Input';
+
+const AccommodationSection = ({ firstInputWidth, secondInputWidth }) => {
+  return (
+    <div className="flex items-center justify-between">
+      <p className="font-medium text-xl -tracking-2">
+        Konaklama Hakkında Bilgiler
+      </p>
+      <div className="flex gap-8">
+        <Input
+          title="Kişi Sayısı"
+          altTitle="Kişi sayısını giriniz"
+          width={firstInputWidth}
+        />
+        <Input
+          title="Konaklama Adresi"
+          altTitle="Konaklama bölgesinin adresini giriniz"
+          width={secondInputWidth}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AccommodationSection;
