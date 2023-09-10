a046d0222f785e5f834c5c403022c9fa79782b2a
Mustafa <mustafamengutayy@gmail.com>
2023-09-10 09:47:53 +0300

    chore: update style

--- src/components/formElements/SubmitSection.jsx
+++ src/components/formElements/SubmitSection.jsx
@@ -2,7 +2,7 @@ import SubmitButton from './SubmitButton';
 
 const SubmitSection = () => {
   return (
-    <div className="flex items-center gap-75 my-12 self-end">
+    <div className="flex items-center gap-72 my-12 self-end">
       <p className="text-base -tracking-2 font-light">
         *Bu alanlar zorunludur.
       </p>
--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div className="text-white min-w-[1000px] font-light -tracking-4 flex items-center justify-between mt-10">
+    <header className="text-white min-w-[1000px] font-light -tracking-4 flex items-center justify-between mt-10">
       <p className="font-semibold text-7">Depremzede</p>
       <div className="flex items-center gap-36 text-base">
         <nav>
@@ -24,7 +24,7 @@ const Header = () => {
           <Link className="bg-grey-1 px-8 rounded-full py-2">Giriş Yap</Link>
         </div>
       </div>
-    </div>
+    </header>
   );
 };
 
