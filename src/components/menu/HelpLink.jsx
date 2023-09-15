3bba2d251bab57a0e1e084ef3fc54226b0d952ef
Mustafa <mustafamengutayy@gmail.com>
2023-09-15 14:10:29 +0300

    chore: add width property

--- src/components/menu/HelpLink.jsx
+++ src/components/menu/HelpLink.jsx
@@ -6,9 +6,9 @@ import { Link } from 'react-router-dom';
  * @param {string} title A title for the link.
  * @returns A Button-like link that can be used to direct people to a Help page.
  */
-const HelpLink = ({ icon, title, route }) => {
+const HelpLink = ({ icon, title, width = '', route }) => {
   return (
-    <Link className="btn-small" to={route}>
+    <Link className="btn-small" to={route} style={{ width: width }}>
       <div className="h-full flex flex-col justify-center items-center gap-2 -tracking-4">
         <img src={icon} />
         <p className="font-semibold text-4 whitespace-nowrap">{title}</p>
