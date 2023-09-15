70439e19c45ecd8915c9715e5751a7c7712b0fe9
Mustafa <mustafamengutayy@gmail.com>
2023-09-15 14:09:54 +0300

    feat: add officer features

--- src/pages/OfficerDashboard.jsx
+++ src/pages/OfficerDashboard.jsx
@@ -3,14 +3,19 @@ import OfficerSideMenu from '../components/officer/OfficerSideMenu';
 
 import officer_profil from '../assets/officer-profile.png';
 import check from '../assets/check.svg';
-import need from '../assets/need.svg';
 
 import search from '../assets/search.svg';
 import map from '../assets/map.svg';
 import info from '../assets/info.svg';
 
+import tent from '../assets/home.svg';
+import clothing from '../assets/clothing-dash.svg';
+import food from '../assets/utensils.svg';
+import pharmacy from '../assets/pharmacy.svg';
+
 import InformationLink from '../components/menu/InformationLink';
 import SideMenuLink from '../components/menu/SideMenuLink';
+import HelpLink from '../components/menu/HelpLink';
 
 const OfficerDashboard = () => {
   useEffect(() => {
@@ -18,8 +23,8 @@ const OfficerDashboard = () => {
   });
 
   return (
-    <div className="flex flex-col items-center">
-      <div className="w-[1300px] -tracking-2 mt-14 flex">
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="w-[1300px] -tracking-2 flex">
         <OfficerSideMenu
           officerImage={officer_profil}
           officerName={'Ahmet Sezer'}
@@ -31,21 +36,31 @@ const OfficerDashboard = () => {
         </div>
 
         <div className="flex flex-col justify-center gap-16 ml-[70px]">
-          <p className="text-white font-semibold text-2xl -tracking-2">
-            İşlemlerim
-          </p>
-          <div className="flex justify-between">
+          <div className="flex justify-between items-center">
             <div className="flex flex-col gap-6 ml-[90px]">
               <InformationLink
                 icon={check}
                 title="Depremzede Bilgi Doğrulama"
-                width="646px"
+                width="650px"
               />
-              <InformationLink
-                icon={need}
-                title="İhtiyaç İsteği"
-                width="646px"
-              />
+              <div>
+                <div className="flex gap-4 mb-6">
+                  <HelpLink icon={tent} title="Çadır İsteği" width="313px" />
+                  <HelpLink
+                    icon={clothing}
+                    title="Giysi İsteği"
+                    width="313px"
+                  />
+                </div>
+                <div className="flex gap-4">
+                  <HelpLink icon={food} title="Gıda İsteği" width="313px" />
+                  <HelpLink
+                    icon={pharmacy}
+                    title="Tıbbi Malzeme İsteği"
+                    width="313px"
+                  />
+                </div>
+              </div>
             </div>
             <div className="mt-14 flex flex-col gap-8 ml-36">
               <SideMenuLink icon={search} title="Arama" />
