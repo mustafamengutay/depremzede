bffbbf1332d7eb21b51b56d07a41a3d2c87202f4
Mustafa <mustafamengutayy@gmail.com>
2023-09-15 14:44:31 +0300

    feat: add path matcher

--- src/components/officer/OfficerSideMenu.jsx
+++ src/components/officer/OfficerSideMenu.jsx
@@ -3,10 +3,19 @@ import officerInfo from '../../assets/bilgiler.svg';
 import officerExit from '../../assets/cikis.svg';
 
 import OfficerAvatar from './OfficerAvatar';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const OfficerSideMenu = ({ officerImage, officerName, jobType }) => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const pathMatchRoute = (route) => {
+    if (route === location.pathname) {
+      return '#2C2C2C';
+    } else {
+      return null;
+    }
+  };
 
   return (
     <div className="w-[270px] flex flex-col items-start">
@@ -19,11 +28,23 @@ const OfficerSideMenu = ({ officerImage, officerName, jobType }) => {
             jobType={jobType}
           />
           <ul className="flex flex-col items-center gap-7 text-white mt-16 w-full">
-            <li className="hover:bg-grey-1 w-full px-8 py-4 rounded-5 flex items-center gap-4 cursor-pointer transition">
+            <li
+              className="hover:bg-grey-1 w-full px-8 py-4 rounded-5 flex items-center gap-4 cursor-pointer transition"
+              style={{
+                backgroundColor: pathMatchRoute('/gorevli-menusu'),
+              }}
+              onClick={() => navigate('/gorevli-menusu')}
+            >
               <img src={officerMenu} alt="Officer Menu" />
               <p className="font-light">İşlemlerim</p>
             </li>
-            <li className="hover:bg-grey-1 px-8 py-4 rounded-5 flex items-center gap-4 cursor-pointer transition">
+            <li
+              className="hover:bg-grey-1 px-8 py-4 rounded-5 flex items-center gap-4 cursor-pointer transition"
+              style={{
+                backgroundColor: pathMatchRoute('/gorevli-bilgileri'),
+              }}
+              onClick={() => navigate('/gorevli-bilgileri')}
+            >
               <img src={officerInfo} alt="Officer Information" />
               <p className="font-light">Bilgilerim</p>
             </li>
