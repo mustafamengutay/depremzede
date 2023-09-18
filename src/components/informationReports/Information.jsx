8df0c4376198b2802383b16a851a6cdbb80c883d
Mustafa <mustafamengutayy@gmail.com>
2023-09-18 17:42:14 +0300

    chore: update radius size

--- src/components/informationReports/Information.jsx
+++ src/components/informationReports/Information.jsx
@@ -19,13 +19,13 @@ const Information = ({ name, address, city, email, phoneNo, status }) => {
 export const StatusTag = ({ statu }) => {
   if (statu) {
     return (
-      <div className="w-[120px] flex justify-center bg-red-200 border border-red-700 text-red-700 rounded-[4px] px-3 py-1">
+      <div className="w-[120px] flex justify-center bg-red-200 border border-red-700 text-red-700 rounded-full px-3 py-1">
         <li>Aktif</li>
       </div>
     );
   } else {
     return (
-      <div className="w-[120px] flex justify-center bg-green-200 border border-green-700 text-green-700 rounded-[4px] px-3 py-1">
+      <div className="w-[120px] flex justify-center bg-green-200 border border-green-700 text-green-700 rounded-full px-3 py-1">
         <li>Tamamlandı</li>
       </div>
     );
