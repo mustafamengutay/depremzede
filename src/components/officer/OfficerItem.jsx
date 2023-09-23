579ec133c10c13d958cce77aaa8c4d42eade8947
Mustafa <mustafamengutayy@gmail.com>
2023-09-23 18:05:09 +0300

    feat: add edit section

--- src/components/officer/OfficerItem.jsx
+++ src/components/officer/OfficerItem.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import edit from '../../assets/edit.svg';
 
 const OfficerItem = React.memo(
   ({ id, name, surname, phoneNumber, email, birth, location, city }) => {
     return (
       <div>
-        <ul className="grid grid-cols-7 justify-items-center items-center text-base font-normal whitespace-nowrap">
+        <ul className="grid grid-cols-8 justify-items-center items-center text-base font-normal whitespace-nowrap">
           <li>{id}</li>
           <li>{name + ' ' + surname}</li>
           <li>{phoneNumber}</li>
@@ -12,6 +13,11 @@ const OfficerItem = React.memo(
           <li>{birth}</li>
           <li>{location}</li>
           <li>{city}</li>
+          <li>
+            <button className="flex flex-col items-center justify-center h-8 w-16  py-2 px-4 bg-grey-1 hover:bg-black transition text-white rounded-full">
+              <img src={edit} width={14} height={14} alt="" />
+            </button>
+          </li>
         </ul>
         <hr className="my-6" />
       </div>
