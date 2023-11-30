28177090284ba70bf6083a1804bfbf2ef0a15919
Mustafa <mustafamengutayy@gmail.com>
2023-11-30 15:35:08 +0200

    feat: add back to previous page functionality

--- src/components/layouts/CloseButton.jsx
+++ src/components/layouts/CloseButton.jsx
@@ -7,6 +7,7 @@ const CloseButton = () => {
       className="h-8 w-8 flex items-center justify-center border border-grey-2 rounded-lg
       hover:bg-gray-200 transition"
       to="/"
+      onClick={() => window.history.back()}
     >
       <img src={cross} alt="A cross" />
     </Link>
