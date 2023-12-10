27159c12f1554549033d3918406950f6bdcafcf1
LordMordekaiser <karakocemir119@gmail.com>
2023-12-10 20:21:46 +0200

    feat: add responsive layout

--- src/pages/ManagerDashboard.jsx
+++ src/pages/ManagerDashboard.jsx
@@ -1,20 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
-import HelpLink from '../components/menu/HelpLink';
-import OfficerSideMenu from '../components/officer/OfficerSideMenu';
-import SideMenuLink from '../components/menu/SideMenuLink';
+import HelpLink from "../components/menu/HelpLink";
+import OfficerSideMenu from "../components/officer/OfficerSideMenu";
+import SideMenuLink from "../components/menu/SideMenuLink";
 
-import search from '../assets/search.svg';
-import map from '../assets/map.svg';
-import info from '../assets/info.svg';
+import search from "../assets/search.svg";
+import map from "../assets/map.svg";
+import info from "../assets/info.svg";
 
-import assignOfficer from '../assets/assignOfficer.svg';
-import createOfficer from '../assets/createOfficer.svg';
-import inventory from '../assets/inventory.svg';
-import officer_profil from '../assets/officer-profile.png';
-import officerRequests from '../assets/officerRequests.svg';
-import { setBackgroundColorBlack } from '../utils/BackgroundColorUtils';
-import { Outlet } from 'react-router-dom';
+import assignOfficer from "../assets/assignOfficer.svg";
+import createOfficer from "../assets/createOfficer.svg";
+import inventory from "../assets/inventory.svg";
+import officer_profil from "../assets/officer-profile.png";
+import officerRequests from "../assets/officerRequests.svg";
+import { setBackgroundColorBlack } from "../utils/BackgroundColorUtils";
+import { Outlet } from "react-router-dom";
 
 const ManagerDashboard = () => {
   useEffect(() => {
@@ -22,17 +22,17 @@ const ManagerDashboard = () => {
   });
 
   return (
-    <div className="container max-w-dashboard h-screen flex items-center justify-between">
+    <div className="container max-w-dashboard h-screen flex items-center justify-between mt-8 mx-4  min-w-[600px]">
       <OfficerSideMenu
         officerImage={officer_profil}
-        officerName={'Ahmet Sezer'}
-        jobType={'Yonetici'}
-        routeAddress={'/yonetici'}
+        officerName={"Ahmet Sezer"}
+        jobType={"Yonetici"}
+        routeAddress={"/yonetici"}
       />
 
       <Outlet />
 
-      <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-8 ">
         <SideMenuLink icon={search} title="Arama" />
         <SideMenuLink icon={map} title="Harita" />
         <SideMenuLink icon={info} title="Bilgilendirme" />
@@ -43,8 +43,8 @@ const ManagerDashboard = () => {
 
 export const ManagerMenu = () => {
   return (
-    <div className="flex flex-col gap-6">
-      <div className="flex gap-6">
+    <div className="flex flex-col gap-6 sm:flex-row mx-2">
+      <div className="flex flex-col gap-6 sm:flex-1  md:mt-0  mt-96">
         <HelpLink
           icon={inventory}
           title="Envanter"
@@ -58,7 +58,7 @@ export const ManagerMenu = () => {
           route="/gorevliler"
         />
       </div>
-      <div className="flex gap-6">
+      <div className="flex flex-col gap-6 sm:flex-1">
         <HelpLink
           icon={createOfficer}
           title="Görevli Oluştur / Sil"
