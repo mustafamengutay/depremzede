1ea8f9aec4418fb3718be6106f293529865b0276
Mustafa <mustafamengutayy@gmail.com>
2023-12-13 14:19:02 +0200

    feat: add cors middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
+
 require('./db/mongoose')
+
 const userRouter = require('./routers/user')
 const officerRouter = require('./routers/officer')
 const governmentRouter = require('./routers/government')
+
 const Officer = require('./models/officer')
 
+const cors = require('cors');
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -24,6 +28,7 @@ app.use((req, res, next) => {
 })
 */
 
+app.use(cors());
 app.use(express.json())
 app.use(userRouter)
 app.use(officerRouter)
