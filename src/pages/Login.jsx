337a7c2fe58051fb411f6ac1fd3b4ec94199c85f
Mustafa <mustafamengutayy@gmail.com>
2023-12-13 10:42:46 +0200

    chore: fix hover style

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -47,7 +47,7 @@ const Login = () => {
             </a>
           </div>
           {/* BURDA HOVER */}
-          <button className='w-48.5 h-10 text-base text-white rounded-xl bg-black px-4 py-2 hover:bg-stone-700	 transition duration-300 ease-in-out'>
+          <button className='w-48.5 h-10 text-base text-white rounded-xl bg-grey-1 hover:bg-black transition duration-300 ease-in-out'>
             Giriş Yap
           </button>
         </div>
