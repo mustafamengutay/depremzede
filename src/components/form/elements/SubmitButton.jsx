19a6e204f2edaad0fb460c6290dea90337450e63
Mustafa <mustafamengutayy@gmail.com>
2023-12-13 10:38:32 +0200

    feat: add event

--- src/components/form/elements/SubmitButton.jsx
+++ src/components/form/elements/SubmitButton.jsx
@@ -4,9 +4,12 @@ import React from 'react';
  *
  * @returns A Submit button can be used inside a form.
  */
-const SubmitButton = () => {
+const SubmitButton = (handleClick) => {
   return (
-    <button className='w-48.5 h-10 text-base text-white rounded-xl bg-grey-1 hover:bg-black px-4 py-2	transition duration-300 ease-in-out'>
+    <button
+      className='w-48.5 h-10 text-base text-white rounded-xl bg-grey-1 hover:bg-black px-4 py-2	transition duration-300 ease-in-out'
+      onClick={handleClick}
+    >
       Gönder
     </button>
   );
