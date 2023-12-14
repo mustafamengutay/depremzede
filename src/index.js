5f904f71bfe882350bc6eadd826cb74a99181adb
Mustafa <mustafamengutayy@gmail.com>
2023-12-14 19:44:33 +0200

    chore: update default font-family

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import '@radix-ui/themes/styles.css';
+import './theme-config.css';
+
 import { Theme } from '@radix-ui/themes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
