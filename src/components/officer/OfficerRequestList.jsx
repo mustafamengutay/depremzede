b44faadbe0bba229c6e5423364bec787b89c6b92
Mustafa <mustafamengutayy@gmail.com>
2023-12-19 10:08:17 +0200

    feat: add officer request listing

--- src/components/officer/OfficerRequestList.jsx
+++ src/components/officer/OfficerRequestList.jsx
@@ -1,40 +1,130 @@
-import React, { useContext, useEffect, useState } from 'react';
-import inventoryRequests from '../../data/inventoryRequests.json';
-import FormContext from '../../context/form/FormContext';
-import OfficerRequestItem from './OfficerRequestItem';
+import { useContext, useEffect, useState } from 'react';
+import { ScrollArea, Table } from '@radix-ui/themes';
+import ReactPaginate from 'react-paginate';
 
-const OfficerRequestList = () => {
-  const [inventories, setInventories] = useState([]);
-  useEffect(() => {
-    setInventories(inventoryRequests);
-  });
+import FormContext from '../../context/form/FormContext';
+
+const OfficerRequestList = ({ posts }) => {
+  const [page, setPage] = useState(0);
+  const [filterData, setFilterData] = useState([]);
+  const records = 8;
 
   const { searchBarValue } = useContext(FormContext);
 
-  const filteredInventory = inventories.filter((inventory) => {
+  useEffect(() => {
+    setFilterData(
+      posts.filter((item, index) => {
+        return (index >= page * records) & (index < (page + 1) * records);
+      })
+    );
+  }, [page, posts]);
+
+  const filteredInventory = filterData.filter((inventory) => {
     if (searchBarValue === '') {
       return inventory;
     } else {
-      const info = inventory.inventoryName + ' ' + inventory.category;
+      const info = inventory.kategori;
       return info.toLowerCase().includes(searchBarValue);
     }
   });
 
+  const handleConfirm = (e, id) => {
+    e.preventDefault();
+
+    // fetch(``, {
+    //   method: 'PATCH',
+    // });
+  };
+
+  const handleCancel = (e, id) => {
+    e.preventDefault();
+
+    // fetch(``, {
+    //   method: 'DELETE',
+    // });
+  };
+
   return (
-    <div className='w-[1160px]'>
-      {filteredInventory.map((inventory, index) => {
-        return (
-          <OfficerRequestItem
-            key={index}
-            officerName={inventory.name}
-            inventoryName={inventory.inventoryName}
-            category={inventory.category}
-            stock={inventory.stock}
-            location={inventory.location}
-          />
-        );
-      })}
+    <div>
+      <ScrollArea scrollbars='horizontal' size='1' type='auto' radius='full'>
+        <Table.Root size={'2'}>
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeaderCell justify={'center'}>
+                Görevli Adı
+              </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell justify={'center'}>
+                Ürün Adı
+              </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell justify={'center'}>
+                Kategori
+              </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell justify={'center'}>
+                Adet Sayısı
+              </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell justify={'center'}>
+                İstenilen Bölge
+              </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell justify={'center'}>
+                Durum
+              </Table.ColumnHeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            {filteredInventory.map((post) => {
+              return (
+                <Table.Row align={'center'} key={post._id}>
+                  <Table.RowHeaderCell justify={'center'}>
+                    {post.gorevliAdi}
+                  </Table.RowHeaderCell>
+                  <Table.Cell justify={'center'}>{post.urunismi}</Table.Cell>
+                  <Table.Cell justify={'center'}>{post.kategori}</Table.Cell>
+                  <Table.Cell justify={'center'}>{post.adetSayisi}</Table.Cell>
+                  <Table.Cell justify={'center'}>
+                    {post.istenilenBolge}
+                  </Table.Cell>
+                  <Table.Cell align='center'>
+                    <div className='flex justify-center gap-2'>
+                      <button
+                        className='flex flex-col items-center justify-center h-8 w-20  py-2 px-4 bg-grey-1 hover:bg-black transition text-white rounded-lg'
+                        onClick={(e) => handleConfirm(e, post._id)}
+                      >
+                        Onayla
+                      </button>
+                      <button
+                        className='flex flex-col items-center justify-center h-8 w-20  py-2 px-4 border border-grey-1 text-black hover:bg-black transition hover:text-white rounded-lg'
+                        onClick={(e) => handleCancel(e, post._id)}
+                      >
+                        İptal et
+                      </button>
+                    </div>
+                  </Table.Cell>
+                </Table.Row>
+              );
+            })}
+          </Table.Body>
+        </Table.Root>
+      </ScrollArea>
+      <ReactPaginate
+        containerClassName='flex gap-2 justify-end mt-10'
+        pageClassName='py-1 px-3 border rounded-lg'
+        activeClassName={'bg-black text-white'}
+        onPageChange={(event) => setPage(event.selected)}
+        pageCount={Math.ceil(posts.length / records)}
+        breakLabel={<LabelButton>{'...'}</LabelButton>}
+        previousLabel={<LabelButton>{'<'}</LabelButton>}
+        nextLabel={<LabelButton>{'>'}</LabelButton>}
+      />
     </div>
+  );
+};
+
+const LabelButton = ({ children }) => {
+  return (
+    <button type='button' className='py-1 px-3 border rounded-lg'>
+      {children}
+    </button>
   );
 };
 
--- src/pages/OfficerRequests.jsx
+++ src/pages/OfficerRequests.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import FormHeader from '../components/form/FormHeader';
 import { setBackgroundColorWhite } from '../utils/BackgroundColorUtils';
 import { resetLocation } from '../utils/ScrollUtils';
@@ -6,38 +6,37 @@ import OfficerRequestList from '../components/officer/OfficerRequestList';
 import SearchBar from '../components/form/SearchBar';
 import Select from '../components/form/elements/Select';
 
+import FormContext from '../context/form/FormContext';
+
 const OfficerRequests = () => {
+  const [officerRequestList, setOfficerRequestList] = useState([]);
+
+  const { getList } = useContext(FormContext);
+
   useEffect(() => {
     // Page Settings
     setBackgroundColorWhite();
     resetLocation();
+
+    const fetchList = async () => {
+      const list = await getList('/gorevli-isteklerini-listele');
+      setOfficerRequestList(list);
+    };
+    fetchList();
   }, []);
 
   const descriptionText =
-    'Aşağıdaki listeden görevliler tarafından yapılan istekleri görebilir, onaylayabilir ve iptal edebilirsiniz.';
+    'Aşağıdaki listeden görevliler tarafından yapılan istekleri görebilir ve onaylayabilirsiniz.';
 
   return (
     <div className='container w-screen h-screen -tracking-4 min-w-[464px]'>
       <FormHeader title='Görevli İstekleri' description={descriptionText} />
-
-      <div className='mt-16 flex justify-between items-center'>
+      <div className='flex justify-between items-center'>
         <SearchBar title='İstenilen ürünün ismini giriniz...' width='300px' />
         <Select />
       </div>
-
       <div className='mt-6 overflow-x-auto'>
-        <ul className='grid grid-cols-6 justify-items-center text-base font-medium min-w-full'>
-          <li>Görevli Adı</li>
-          <li>Ürün İsmi</li>
-          <li>Kategori</li>
-          <li>Adet Sayısı</li>
-          <li>İstenilen Bölge</li>
-          <li>İşlem</li>
-        </ul>
-        <hr className='my-6' />
-        <div>
-          <OfficerRequestList />
-        </div>
+        <OfficerRequestList posts={officerRequestList} />
       </div>
     </div>
   );
