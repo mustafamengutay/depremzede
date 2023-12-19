4586a4b63e90862de7afbd5885ad17effa735cf6
Mustafa <mustafamengutayy@gmail.com>
2023-12-19 10:45:11 +0200

    feat: add inventory listing & update table

--- src/components/inventory/InventoryList.jsx
+++ src/components/inventory/InventoryList.jsx
@@ -1,39 +1,121 @@
-import React, { useContext, useEffect, useState } from 'react';
-import InventoryItem from './InventoryItem';
-import data from '../../data/inventories.json';
+import { useContext, useEffect, useState } from 'react';
+import { ScrollArea, Table } from '@radix-ui/themes';
+import ReactPaginate from 'react-paginate';
+
 import FormContext from '../../context/form/FormContext';
 
-const InventoryList = () => {
-  const [inventories, setInventories] = useState([]);
-  useEffect(() => {
-    setInventories(data);
-  }, []);
+const OfficerRequestList = ({ posts }) => {
+  const [page, setPage] = useState(0);
+  const [filterData, setFilterData] = useState([]);
+  const records = 8;
 
   const { searchBarValue } = useContext(FormContext);
 
-  const filteredInventories = inventories.filter((inventory) => {
+  useEffect(() => {
+    setFilterData(
+      posts.filter((item, index) => {
+        return (index >= page * records) & (index < (page + 1) * records);
+      })
+    );
+  }, [page, posts]);
+
+  const filteredInventory = filterData.filter((inventory) => {
     if (searchBarValue === '') {
       return inventory;
     } else {
-      return inventory.name.toLowerCase().includes(searchBarValue);
+      const info = inventory.inventoryName;
+      return info.toLowerCase().includes(searchBarValue);
     }
   });
 
+  const handleConfirm = (e, id) => {
+    e.preventDefault();
+
+    // fetch(``, {
+    //   method: 'PATCH',
+    // });
+  };
+
+  const handleCancel = (e, id) => {
+    e.preventDefault();
+
+    // fetch(``, {
+    //   method: 'DELETE',
+    // });
+  };
+
   return (
     <div>
-      {filteredInventories.map((inventory, index) => {
-        return (
-          <InventoryItem
-            key={index}
-            inventoryName={inventory.name}
-            category={inventory.category}
-            stock={inventory.stock}
-            unit={inventory.unit}
-          />
-        );
-      })}
+      <ScrollArea scrollbars='horizontal' size='1' type='auto' radius='full'>
+        <Table.Root size={'2'}>
+          <Table.Header>
+            <Table.Row align={'center'}>
+              <Table.ColumnHeaderCell justify={'center'}>
+                Ürün Adı
+              </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell justify={'center'}>
+                Kategori
+              </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell justify={'center'}>
+                Ürün Türü
+              </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell justify={'center'}>
+                Adet Sayısı
+              </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell justify={'center'}>
+                Açıklama
+              </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell justify={'center'}>
+                Durum
+              </Table.ColumnHeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            {filteredInventory.map((post) => {
+              return (
+                <Table.Row align={'center'} key={post._id}>
+                  <Table.RowHeaderCell justify={'center'}>
+                    {post.inventoryName}
+                  </Table.RowHeaderCell>
+                  <Table.Cell justify={'center'}>{post.category}</Table.Cell>
+                  <Table.Cell justify={'center'}>{post.type}</Table.Cell>
+                  <Table.Cell justify={'center'}>{post.stock}</Table.Cell>
+                  <Table.Cell justify={'center'}>{post.description}</Table.Cell>
+                  <Table.Cell align='center'>
+                    <button
+                      className='flex flex-col items-center justify-center h-8 w-20  py-2 px-4 bg-grey-1 hover:bg-black transition text-white rounded-lg'
+                      onClick={(e) => handleConfirm(e, post._id)}
+                    >
+                      Azalt
+                    </button>
+                  </Table.Cell>
+                </Table.Row>
+              );
+            })}
+          </Table.Body>
+        </Table.Root>
+      </ScrollArea>
+      <ReactPaginate
+        containerClassName='flex gap-2 justify-end mt-10'
+        pageClassName='py-1 px-3 border rounded-lg'
+        activeClassName={'bg-black text-white'}
+        onPageChange={(event) => setPage(event.selected)}
+        pageCount={Math.ceil(posts.length / records)}
+        breakLabel={<LabelButton>{'...'}</LabelButton>}
+        previousLabel={<LabelButton>{'<'}</LabelButton>}
+        nextLabel={<LabelButton>{'>'}</LabelButton>}
+      />
     </div>
   );
 };
 
-export default InventoryList;
+const LabelButton = ({ children }) => {
+  return (
+    <button type='button' className='py-1 px-3 border rounded-lg'>
+      {children}
+    </button>
+  );
+};
+
+export default OfficerRequestList;
--- src/pages/InventoriesList.jsx
+++ src/pages/InventoriesList.jsx
@@ -1,47 +1,45 @@
-import { useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import FormHeader from '../components/form/FormHeader';
 import { setBackgroundColorWhite } from '../utils/BackgroundColorUtils';
 import { resetLocation } from '../utils/ScrollUtils';
-import InventoryList from '../components/inventory/InventoryList';
 import SearchBar from '../components/form/SearchBar';
 import Select from '../components/form/elements/Select';
 
-const InventoriesList = () => {
-  // TODO: We can add a search bar
+import FormContext from '../context/form/FormContext';
+import InventoryList from '../components/inventory/InventoryList';
+
+const OfficerRequests = () => {
+  const [inventoryList, setInventoryList] = useState([]);
+
+  const { getList } = useContext(FormContext);
 
   useEffect(() => {
     // Page Settings
     setBackgroundColorWhite();
     resetLocation();
+
+    const fetchList = async () => {
+      const list = await getList('/inventory');
+      setInventoryList(list);
+    };
+    fetchList();
   }, []);
 
   const descriptionText =
     'Aşağıdaki listeden depoda bulunan envanterleri görebilir ve ekleme/çıkarma işlemleri yapabilirsiniz.';
 
   return (
-    <div className='container w-screen h-screen -tracking-4'>
+    <div className='container w-screen h-screen -tracking-4 min-w-[464px]'>
       <FormHeader title='Envanter' description={descriptionText} />
-
       <div className='flex justify-between items-center'>
-        <SearchBar title='Ürün ismini giriniz...' width='300px' />
+        <SearchBar title='İstenilen ürünün ismini giriniz...' width='300px' />
         <Select />
       </div>
-
-      <div className='mt-6'>
-        <ul className='grid grid-cols-5 justify-items-center text-base font-medium'>
-          <li>Ürün İsmi</li>
-          <li>Kategori</li>
-          <li>Adet Sayısı</li>
-          <li>Ölçü Birimi</li>
-          <li>İşlem</li>
-        </ul>
-        <hr className='my-6' />
-        <div>
-          <InventoryList />
-        </div>
+      <div className='mt-6 overflow-x-auto'>
+        <InventoryList posts={inventoryList} />
       </div>
     </div>
   );
 };
 
-export default InventoriesList;
+export default OfficerRequests;
