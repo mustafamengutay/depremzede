b64ce5c75d888a3b1a44df754ebd882a23fbb6ed
Mustafa <mustafamengutayy@gmail.com>
2023-12-21 14:16:44 +0200

    chore: update selection & value

--- src/components/form/elements/Select.jsx
+++ src/components/form/elements/Select.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
-const Select = () => {
+const Select = ({ options, id, set }) => {
   return (
-    <select className='border border-input rounded-xl px-3 py-1 text-base h-10 bg-white'>
-      <option value='default'>İsimleri Sırala</option>
-      <option value='bk'>Büyükten Küçüğe</option>
-      <option value='kb'>Küçükten Büyüğe</option>
+    <select
+      className='flex h-10 w-full items-center justify-between whitespace-nowrap rounded-xl border border-input bg-transparent px-3 py-1 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1'
+      onChange={(e) => set(e.target.value)}
+      placeholder='400'
+      value={id}
+    >
+      {options.map((option) => {
+        return (
+          <option value={Object.keys(option)[0]}>
+            {Object.values(option)[0]}
+          </option>
+        );
+      })}
     </select>
   );
 };
