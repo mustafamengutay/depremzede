9300991efc756e8dfd88df3686acb102eea5858e
Mustafa <mustafamengutayy@gmail.com>
2023-12-24 17:36:27 +0200

    chore: add pulse

--- src/components/menu/HelpLink.jsx
+++ src/components/menu/HelpLink.jsx
@@ -1,16 +1,16 @@
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 /**
  * @param {string} icon A path for an icon.
  * @param {string} title A title for the link.
  * @returns A Button-like link that can be used to direct people to a Help page.
  */
-const HelpLink = ({ icon, title, width = "", route, onClick }) => {
+const HelpLink = ({ icon, title, width = '', route, onClick }) => {
   return (
     <Link
       onClick={onClick}
-      className='btn-small hover:bg-white-2 hover:shadow-inner transition duration-500 ease-in-out'
+      className='btn-small hover:bg-white-2 hover:shadow-inner transition-transform duration-250 transform hover:scale-95 hover:shadow-custom hover:animate-pulse-custom'
       to={route}
       style={{ width: width }}
     >
